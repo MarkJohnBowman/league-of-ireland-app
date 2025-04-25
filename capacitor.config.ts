5b8b6c37d3ca0def3c1044c262a5db20a399cb61
MarkJohnBowman <markjohnbowman223@gmail.com>
2025-04-25 13:31:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'league-of-ireland-app',
+  webDir: 'www'
+};
+
+export default config;
